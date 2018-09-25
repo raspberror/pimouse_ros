0f311b9cff2275996626a7347b33edf7b60b67a2
Raspberrors Are Go <raspberror@welpal.com>
2018-09-25 13:11:44 +0000

    modified link of buzzer.py

--- test/travis_prepare_dummy_files.bash
+++ test/travis_prepare_dummy_files.bash
@@ -6,9 +6,6 @@ echo "0 0 0 0" | sudo tee /dev/rtlightsensor0
 sudo chmod 666 /dev/rtlightsensor0
 echo "0" | sudo tee /dev/rtswitch{0,1,2}
 sudo chmod 666 /dev/rtswitch{0,1,2}
-ls -al /dev/rt*
-echo 1234 > /dev/rtbuzzer0
-cat /dev/rtbuzzer0
 
 # Copyright 2016 Ryuichi Ueda
 # Released under the BSD License.
